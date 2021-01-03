b997e9dc341c61a1c8770a0de7f75aec52a46927
Sam Ping <samuelping42@gmail.com>
2021-01-03 07:06:19 -0500

    modify automatic deployment message; fix typo in cms config

--- client/deploy.sh
+++ client/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 npm run export
-echo "# Hi! You're looking at the build artifacts of my website here. To check out the code, switch over to the Main branch! Note: This message was automatically generated." > out/README.md
+echo "# Hi!" > out/README.md
+echo "You're currently looking at the build artifacts of my website. To check out the code, switch over to the Main branch! Note: This message was automatically generated." >> out/README.md
 git add out
 git commit -m "deploy to gh-pages"
 cd ..
