772fe2a798c6ea5edc6074cad94215885c7881aa
Sam Ping <samuelping42@gmail.com>
2021-01-07 22:38:56 -0500

    testing

--- server/index.js
+++ server/index.js
@@ -1,72 +1,68 @@
-require("dotenv").config();
 const express = require("express");
 const tiny = require("tiny-json-http");
 
+const client_id = process.env.GITHUB_OAUTH_CLIENT_ID;
+const client_secret = process.env.GITHUB_OAUTH_SECRET_KEY;
+const authUrl = `https://github.com/login/oauth/authorize?client_id=${client_id}&scope=repo,user`;
+const tokenUrl = "https://github.com/login/oauth/access_token";
+
 const app = express();
 
-const github_client_id = process.env.GITHUB_OAUTH_CLIENT_ID;
-const github_secret_key = process.env.GITHUB_OAUTH_SECRET_KEY;
-const authURL = "https://github.com/login/oauth/authorize?client_id="
-  .concat(github_client_id)
-  .concat("&scope=repo,user");
-const tokenURL = "https://github.com/login/oauth/access_token";
-
-// if someone tries to views this
+// NetlifyCMS doesn't use this root page. It's only for dev purposes
 app.get("/", (req, res) => {
-  res.send("Nothing to see here! *whistles*");
+  res.send(`<a href="${authUrl}">Login with Github</a>`);
 });
 
-// Netlify CMS makes a request to /auth, so I redirect the user to GitHub login
-app.get("/auth", (req, res) => {
-  console.log("Redirecting to: ", authURL);
-  res.redirect(authURL);
-});
+// NetlifyCMS expects to land on a page at /auth.
+app.get("/auth", (req, res) => res.redirect(authUrl));
 
-// after logging in, GitHub redirects to here
 app.get("/callback", async (req, res) => {
   const data = {
     code: req.query.code,
-    github_client_id,
-    github_secret_key,
+    client_id,
+    client_secret,
   };
 
   try {
     const { body } = await tiny.post({
-      url: tokenURL,
+      url: tokenUrl,
       data,
       headers: {
+        // GitHub returns a string by default, ask for JSON to make the reponse easier to parse.
         Accept: "application/json",
       },
     });
 
-    const postMessageContent = {
+    const postMsgContent = {
       token: body.access_token,
       provider: "github",
     };
 
+    // This is what talks to the NetlifyCMS page. Using window.postMessage we give it the
+    // token details in a format it's expecting
     const script = `
     <script>
-        (function() {
-            function receiveMessage(e) {
-                console.log("receiveMessage %o", e);
+    (function() {
+      function recieveMessage(e) {
+        console.log("recieveMessage %o", e);
         
-                // send message to main window with the app
-                window.opener.postMessage(
-                    'authorization:github:success:${JSON.stringify(
-                      postMessageContent
-                    )}', 
-                    e.origin
-                );
-            }
+        // send message to main window with the app
+        window.opener.postMessage(
+          'authorization:github:success:${JSON.stringify(postMsgContent)}', 
+          e.origin
+        );
+      }
 
-            window.addEventListener("message", receiveMessage, false);
-            window.opener.postMessage("authorizing:github", "*");
-        })()
+      window.addEventListener("message", recieveMessage, false);
+      window.opener.postMessage("authorizing:github", "*");
+    })()
     </script>`;
 
     return res.send(script);
   } catch (err) {
+    // If we hit an error we'll handle that here
     console.log(err);
+    res.redirect("/?error=😡");
   }
 });
 
