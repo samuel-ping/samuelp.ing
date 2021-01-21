0a0ccecf973d573383ef9fd5b3e2aeddbd5a7b61
Sam Ping <samuelping42@gmail.com>
2021-01-21 02:19:19 -0500

    remove trailing slash from next config

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   basePath: "/personal-website-v3",
-  assetPrefix: "/personal-website-v3/",
+  assetPrefix: "/personal-website-v3",
   target: "serverless",
   webpack: function (config) {
     config.module.rules.push({
