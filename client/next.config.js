7151e77e75c6665f99c101a4f83541907fdd6958
Sam Ping <samuelping42@gmail.com>
2021-05-10 00:19:16 -0400

    Use custom loader to fix broken export

--- client/next.config.js
+++ client/next.config.js
@@ -2,6 +2,11 @@ module.exports = {
   future: {
     webpack5: true,
   },
+  // fixes broken next export during deployment
+  images: {
+    loader: 'imgix',
+    path: '',
+  },
   webpack: function (config) {
     config.module.rules.push({
       test: /\.md$/,
