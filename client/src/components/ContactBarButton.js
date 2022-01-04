adefa4ea20668b68fe685df7f1637988011c464c
Sam Ping <samuelping42@gmail.com>
2022-01-04 04:15:21 -0500

    Fix contact bar for mobile

--- client/src/components/ContactBarButton.js
+++ client/src/components/ContactBarButton.js
@@ -3,11 +3,14 @@ const ContactBarButton = ({
   url,
   icon,
 }) => (
-  <a href={url} target="_blank" rel="noopener noreferrer">
-    <span className="flex rounded-full transition-colors border-2 border-green-600 hover:bg-green-600 fill-current hover:text-green-50 p-8 justify-center content-center cursor-pointer">
-      {icon}
-    </span>
-  </a>
+  <>
+    <a href={url} target="_blank" rel="noopener noreferrer">
+      {/* <span className="basis-1/2 flex justify-center items-center rounded-full h-28 w-28 md:h-32 md:w-32 m-4 cursor-pointer transition-colors border-2 border-green-600 hover:bg-green-600 fill-current hover:text-green-50"> */}
+      <span className="cursor-pointer flex justify-center items-center h-28 w-28 md:h-32 md:w-32 rounded-full transition-colors border-2 border-green-600 hover:bg-green-600 fill-current hover:text-green-50">
+        {icon}
+      </span>
+    </a>
+  </>
 );
 
 export default ContactBarButton;
