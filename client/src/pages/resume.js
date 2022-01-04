1f204abc2840983fc05c2ee4e0870128fd5a5382
Sam Ping <samuelping42@gmail.com>
2022-01-04 05:49:57 -0500

    Fix up resume page for mobile (by adding warnings for small text)

--- client/src/pages/resume.js
+++ client/src/pages/resume.js
@@ -16,11 +16,20 @@ export default function Resume({ resumePath, lastUpdated }) {
 
   return (
     <div className="flex flex-col justify-center items-center max-w-full">
+      <div className="font-semibold italic sm:hidden">
+        NOTE: Hey, thanks for (trying to) view my resume! Your screen looks a
+        little small though, so rather than strain your eyes, you should
+        probably download or open it in a new tab by tapping one of the
+        following buttons:
+      </div>
       <div className="flex flex-row items-center justify-between min-w-full">
         <span className="justify-self-start font-light italic">
           Last Updated: {lastUpdated}
         </span>
         <div className="flex flex-row justify-self-end">
+          <span className="font-bold leading-none sm:hidden">
+            Download<span className="text-3xl">&rarr;</span>
+          </span>
           <a
             href={resumePath}
             download
@@ -28,6 +37,9 @@ export default function Resume({ resumePath, lastUpdated }) {
           >
             <DownloadIcon size={iconSize} />
           </a>
+          <span className="font-bold leading-none sm:hidden">
+            New Tab<span className="text-3xl">&rarr;</span>
+          </span>
           <a
             href={resumePath}
             target="_blank"
@@ -39,7 +51,7 @@ export default function Resume({ resumePath, lastUpdated }) {
         </div>
       </div>
 
-      <PDFViewer url={resumePath} width={900} />
+      <PDFViewer url={resumePath} width={1000} />
     </div>
   );
 }
