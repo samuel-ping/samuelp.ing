3c687ba7b26c144c0514134c917d224aacae0acd
Sam Ping <samuelping42@gmail.com>
2022-01-04 06:03:59 -0500

    Make minor adjustments to about me page to better fit mobile devices

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -5,7 +5,7 @@ import mdToReact from '@lib/MarkdownProcessor';
 
 export default function About({ description, profilePicture }) {
   return (
-    <article className="prose prose-2xl prose-green">
+    <article className="prose prose-sm mx-4 my-2 sm:prose-2xl sm:m-0">
       <h1>About Me</h1>
       <Avatar imgSrc={profilePicture} alt="Photo of Sam" size={200} />
       {/* <Description content={description} /> */}
