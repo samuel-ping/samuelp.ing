fe51016d030eec21bb4ac1cdfb28fa454d534d59
Sam Ping <samuelping42@gmail.com>
2022-01-05 21:56:33 -0500

    Turn into PWA

--- client/next.config.js
+++ client/next.config.js
@@ -1,21 +1,36 @@
 const { withPlaiceholder } = require('@plaiceholder/next');
 const withPlugins = require('next-compose-plugins');
+const withPWA = require('next-pwa');
 
-module.exports = withPlugins([[withPlaiceholder]], {
-  future: {
-    webpack5: true,
+module.exports = withPlugins(
+  [
+    [withPlaiceholder],
+    [
+      withPWA,
+      {
+        pwa: {
+          dest: 'public',
+          disable: process.env.NODE_ENV === 'development',
+        },
+      },
+    ],
+  ],
+  {
+    future: {
+      webpack5: true,
+    },
+    // Fixes broken next export for next/image: https://github.com/vercel/next.js/issues/21079
+    images: {
+      domains: ['avatars.githubusercontent.com'],
+      loader: 'imgix',
+      path: '',
+    },
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.md$/,
+        use: 'raw-loader',
+      });
+      return config;
+    },
   },
-  // Fixes broken next export for next/image: https://github.com/vercel/next.js/issues/21079
-  images: {
-    domains: ['avatars.githubusercontent.com'],
-    loader: 'imgix',
-    path: '',
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.md$/,
-      use: 'raw-loader',
-    });
-    return config;
-  },
-});
+);
