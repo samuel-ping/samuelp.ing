b535f39f874835b2c8038eb511669abef66aded7
Samuel Ping <55605862+samuel-ping@users.noreply.github.com>
2025-05-24 11:21:04 -0400

    more responsive 404

--- components/notFoundMessage.js
+++ components/notFoundMessage.js
@@ -12,11 +12,11 @@ export default function NotFoundMessage() {
           alt="photo of Shrimp, my favorite succulent"
           placeholder="blur"
           priority
-          className="w-64 h-64 animate-[spin_6s_linear_infinite]"
+          className="w-44 h-44 md:w-64 md:h-64 animate-[spin_6s_linear_infinite]"
         />
         <span className="text-8xl font-extrabold">4</span>
       </div>
-      <span className="text-xl">
+      <span className="text-xl mx-6">
         The page you&apos;re looking for does not exist. Instead, here&apos;s
         Shrimp, my <span className="italic">echeveria fleur blanc</span> :&#41;
       </span>
