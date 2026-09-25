dec8c953376be2360f73f292eabea4ef41d52ac1
Samuel Ping <sam.ping@mongodb.com>
2026-09-25 18:48:39 -0400

    fix accent color

--- components/navbarButton.js
+++ components/navbarButton.js
@@ -8,7 +8,7 @@ const NavbarButton = ({ active, title, route, mobile, close }) => {
       <Link
         href={route}
         className={`${
-          active ? 'text-green-400 dark:text-green-50' : ''
+          active ? 'text-green-400 dark:text-green-100' : ''
         } p-12 text-center text-xl transition-colors hover:text-green-400 dark:hover:text-green-50`}
         onClick={close}
         aria-hidden="true"
@@ -22,7 +22,7 @@ const NavbarButton = ({ active, title, route, mobile, close }) => {
   return (
     <div
       className={`${
-        active ? 'text-green-400 dark:text-green-50' : ''
+        active ? 'text-green-400 dark:text-green-100' : ''
       } flex items-center justify-center cursor-pointer text-xl transition-colors hover:text-green-400 dark:hover:text-green-50`}
     >
       <Link href={route} data-umami-event={`${title} nav button`}>
